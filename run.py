e5b33e456d942518d85e9b0bed17d94c355cf6f6
Lilla Kavecsanszki <lilla-kavecsanszki@outlook.com>
2023-02-24 12:17:03 +0000

    Create get_current_stock_data function to get data string from the user

--- run.py
+++ run.py
@@ -17,6 +17,21 @@ wines = SHEET.worksheet('wines')
 beers = SHEET.worksheet('beers')
 soft_drinks = SHEET.worksheet('soft_drinks')
 
-spirit_data = spirits.get_all_values()
+spirit_stock = spirits.get_all_values()
+wine_stock = wines.get_all_values()
+beer_stock = beers.get_all_values()
+soft_drink_stock= soft_drinks.get_all_values()
 
-print(spirit_data)
+#wines
+def get_current_stocks_data():
+    """
+    Get current stock holoding figures input from the user
+    """
+    print("Please enter current stock data.") 
+    print("Data should be 7 numbers, separated by commas.")
+    print("Examlpe: 12,23,34,36,46,37,49\n")
+
+    data_str = input("Enter your numbers here: ")
+    print(f"The current stocks holding is {data_str}")
+
+get_current_stocks_data()
